b6825983b0a8d7bc6670ccca747d494da9cd3362
smjsih@naver.com <smjsih@naver.com>
2022-08-29 04:07:52 +0900

    Feat :

--- src/main/java/com/example/helpcs/HelpCsApplication.java
+++ src/main/java/com/example/helpcs/HelpCsApplication.java
@@ -2,13 +2,18 @@ package com.example.helpcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class HelpCsApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(HelpCsApplication.class, args);
+        SpringApplication application = new SpringApplication(HelpCsApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
+
     }
 
 }
