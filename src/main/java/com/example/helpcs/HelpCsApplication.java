046312b339464019b1aad4ed9e6ea924a6b2e6b6
MyoungJinSeo <sih5673@gmail.com>
2023-07-10 20:40:22 +0900

    refactor : application.run(args);

--- src/main/java/com/example/helpcs/HelpCsApplication.java
+++ src/main/java/com/example/helpcs/HelpCsApplication.java
@@ -2,7 +2,6 @@ package com.example.helpcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -10,10 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class HelpCsApplication {
 
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(HelpCsApplication.class);
-        application.addListeners(new ApplicationPidFileWriter());
-        application.run(args);
-
+        SpringApplication.run(HelpCsApplication.class, args);
     }
 
 }
